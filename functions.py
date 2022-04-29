eaee2d35e2a12761e9f85667509f6a5dc5576943
Miguel <miguel.ramirez@ohmyfi.com>
2022-04-29 17:44:45 -0500

    funciones nuevas implementadas

--- functions.py
+++ functions.py
@@ -3,9 +3,10 @@ def assign_range(server_id : str, limit_algorithm: int):
     modified_range = f"({server_id}, {limit_algorithm}) U [0, {server_id}]"
     return server_range, modified_range
 
-def print_ranges(server_id : str, server_range : str, modified_range : str):
+def print_ranges(server_id : str, successor : str, server_range : str, modified_range : str):
     msg = f"""
     Identificador del servidor : {server_id}
+    sucesor : {successor}
     Rango establecido : {server_range}
     Rango modificado : {modified_range}
     """
@@ -13,3 +14,28 @@ def print_ranges(server_id : str, server_range : str, modified_range : str):
 
 def get_ports(url: str):
     return f"tcp://*:{url}", f"tcp://localhost:{url}"
+
+def get_range(modified_range : str, start_character : str):
+    items = {'(' : ')', '[' : ']'}
+    start_index = modified_range.find(start_character)
+    string = ''
+    while True:
+        if modified_range[start_index] != items[start_character]:
+            string += modified_range[start_index]
+        else:
+            break
+        start_index += 1
+
+    string += items[start_character]
+    return string
+
+def review_responsibility(new_node_identifier : int , modified_range : str):
+    if 'U' in modified_range:
+        # es porque tenemos dos rangos
+        pass
+    else:
+        # es porque solo tenemos un rango
+        pass
+
+
+#get_range(f"(29, 64) U [0, 29]", '[')
--- server.py
+++ server.py
@@ -15,6 +15,7 @@ class Server:
         self.server_id = server_id
         self.url = url
         self.cmd = cmd
+        self.home_url = home_url
 
         #Atributos para las  peticiones
 
@@ -30,18 +31,28 @@ class Server:
     def start(self):
         self.socket_response.bind(get_ports(self.url)[0])
         while True:
-            print_ranges(self.server_id, self.server_range, self.modified_range)
+            print_ranges(self.server_id, self.successor, self.server_range, self.modified_range)
             message = self.socket_response.recv_multipart()
-
+            if message[0].decode() == 'i_am_responsible':
+                review_responsibility(int(message[1].decode()), self.modified_range)
+                # necesitamos dar respuesta
 
     def turn_on(self):
         if self.cmd == '--first':
             # significa que es el primer servidor
             self.server_range, self.modified_range = assign_range(self.server_id, self.LIMIT_ALGORITHM)
+            self.successor = get_ports(self.url)[1]
             self.start()
-        else:
-            # signfica que ya existe un servidor primero
-            pass
+
+        elif self.cmd == '--conect':
+            if self.home_url is not None:
+                #Me conecto al servidor que yo especifique
+                self.socket_request.connect(get_ports(self.home_url)[1])
+                #Despues debo pregunta si es responsable de guardarme
+                self.socket_request.send_multipart(
+                    ['i_am_responsible'.encode(), self.server_id.encode()]
+                )
+                message = self.socket_request.recv()
 
 if __name__ == '__main__':
     server_id = sys.argv[1]
